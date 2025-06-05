2af401782cfaf52f21141b3c24cbf925643c2f6e
Richard Tanaka <kendy_t88@hotmail.com>
2025-06-05 18:01:37 -0300

    [2] - Building REST APIs - On going #3

--- DevHabit/DevHabit.Api/Controllers/HabitsController.cs
+++ DevHabit/DevHabit.Api/Controllers/HabitsController.cs
@@ -1,6 +1,8 @@
-﻿using DevHabit.Api.Database;
+﻿using System.Linq.Expressions;
+using DevHabit.Api.Database;
 using DevHabit.Api.DTOs.Habits;
 using DevHabit.Api.Entities;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,38 +13,11 @@ namespace DevHabit.Api.Controllers;
 public sealed class HabitsController(ApplicationDbContext dbContext) : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<HabitsCollectionDto>> GetHabits()
+    public async Task<ActionResult<HabitsCollectionDto>> GetHabit()
     {
         List<HabitDto> habits = await dbContext
             .Habits
-            .Select(h => new HabitDto
-            {
-                Id = h.Id,
-                Name = h.Name,
-                Description = h.Description,
-                Type = h.Type,
-                Frequency = new FrequencyDto
-                {
-                    Type = h.Frequency.Type,
-                    TimesPerPeriod = h.Frequency.TimesPerPeriod
-                },
-                Target = new TargetDto
-                {
-                    Value = h.Target.Value,
-                    Unit = h.Target.Unit
-                },
-                Status = h.Status,
-                IsArchived = h.IsArchived,
-                EndDate = h.EndDate,
-                Milestone = h.Milestone != null ? new MilestoneDto
-                {
-                    Target = h.Milestone.Target,
-                    Current = h.Milestone.Current
-                } : null,
-                CreatedAtUtc = h.CreatedAtUtc,
-                UpdatedAtUtc = h.UpdatedAtUtc,
-                LastCompletedAtUtc = h.LastCompletedAtUtc
-            }).
+            .Select(HabitQueries.ProjectToDto()).
             ToListAsync();
         var habitsCollectionDto = new HabitsCollectionDto
         {
@@ -50,40 +25,15 @@ public sealed class HabitsController(ApplicationDbContext dbContext) : Controlle
         };
         return Ok(habitsCollectionDto);
     }
+
+
     [HttpGet("{id}")]
     public async Task<ActionResult<HabitDto>> GetHabitId(string id)
     {
         HabitDto? habit = await dbContext
             .Habits
             .Where(h => h.Id == id)
-            .Select(h => new HabitDto
-            {
-                Id = h.Id,
-                Name = h.Name,
-                Description = h.Description,
-                Type = h.Type,
-                Frequency = new FrequencyDto
-                {
-                    Type = h.Frequency.Type,
-                    TimesPerPeriod = h.Frequency.TimesPerPeriod
-                },
-                Target = new TargetDto
-                {
-                    Value = h.Target.Value,
-                    Unit = h.Target.Unit
-                },
-                Status = h.Status,
-                IsArchived = h.IsArchived,
-                EndDate = h.EndDate,
-                Milestone = h.Milestone != null ? new MilestoneDto
-                {
-                    Target = h.Milestone.Target,
-                    Current = h.Milestone.Current
-                } : null,
-                CreatedAtUtc = h.CreatedAtUtc,
-                UpdatedAtUtc = h.UpdatedAtUtc,
-                LastCompletedAtUtc = h.LastCompletedAtUtc
-            })
+            .Select(HabitQueries.ProjectToDto())
             .FirstOrDefaultAsync();
 
         if (habit == null)
@@ -92,15 +42,80 @@ public sealed class HabitsController(ApplicationDbContext dbContext) : Controlle
         }
         return Ok(habit);
     }
-    /*[HttpPost]
-    public IActionResult CreateHabit([FromBody] Habit habit)
+    [HttpPost]
+    public async Task<ActionResult<HabitDto>> CreateHabit(CreateHabitDto createHabitDto)
     {
+        Habit habit = createHabitDto.ToEntity();
+        
+        dbContext.Habits.Add(habit);
+                
+        await dbContext.SaveChangesAsync();
+        
+        HabitDto habitDto = habit.ToDto();
+        
+        return CreatedAtAction(nameof(GetHabit), new { id = habitDto.Id}, habitDto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateHabit(string id, UpdateHabitDto updateHabitDto)
+    {
+        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+        
         if (habit == null)
         {
-            return BadRequest("Habit cannot be null.");
+            return NotFound();
         }
-        dbContext.Habits.Add(habit);
-        dbContext.SaveChanges();
-        return CreatedAtAction(nameof(GetHabits), new { id = habit.Id }, habit);
-    }*/
+        
+        habit.UpdateFromDto(updateHabitDto);
+        
+        await dbContext.SaveChangesAsync();        
+      
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<ActionResult> PatchHabit(string id, JsonPatchDocument<HabitDto> patchDocument)
+    {
+        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+        
+        if (habit == null)
+        {
+            return NotFound();
+        }
+        
+        HabitDto habitDto = habit.ToDto();
+        
+        patchDocument.ApplyTo(habitDto, ModelState);
+        
+        if (!TryValidateModel(habitDto))
+        {
+            return ValidationProblem(ModelState);
+        }
+        
+        habit.Name = habitDto.Name;
+        habit.Description = habitDto.Description;
+        habit.UpdatedAtUtc = DateTime.UtcNow;
+
+        await dbContext.SaveChangesAsync();
+        
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteHabit(string id)
+    {
+        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+        
+        if (habit == null)
+        {
+            return NotFound();
+        }
+        
+        dbContext.Habits.Remove(habit);
+        
+        await dbContext.SaveChangesAsync();
+        
+        return NoContent();
+    }
+
 }
