684e28b583f945cab9e8ae3b1ef3b6bfc9edda41
Richard Tanaka <kendy_t88@hotmail.com>
2025-06-19 10:21:24 -0300

    [4] - Authentication and Authorization - On going #4

--- DevHabit/DevHabit.Api/Database/ApplicationIdentityDbContext.cs
+++ DevHabit/DevHabit.Api/Database/ApplicationIdentityDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using DevHabit.Api.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
