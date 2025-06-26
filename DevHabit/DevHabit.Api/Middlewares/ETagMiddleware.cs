a3890f469f2cb318071f80f402e592057e8d099c
Richard Tanaka <kendy_t88@hotmail.com>
2025-06-26 18:23:27 -0300

    [6] - Advanced REST API Concerns - Optimistic Locking, Concurrency Control - Finished

--- DevHabit/DevHabit.Api/Middlewares/ETagMiddleware.cs
+++ DevHabit/DevHabit.Api/Middlewares/ETagMiddleware.cs
@@ -6,6 +6,11 @@ namespace DevHabit.Api.Middlewares;
 
 public sealed class ETagMiddleware(RequestDelegate next)
 {
+    private static readonly string[] ConcurrencyCheckMethods = 
+    {
+        HttpMethods.Put,
+        HttpMethods.Patch
+    };
     public async Task InvokeAsync(HttpContext context, InMemoryETagStore etagStore)
     {
         if (CanSkipETag(context))
@@ -17,6 +22,19 @@ public sealed class ETagMiddleware(RequestDelegate next)
 
         string resourceUri = context.Request.Path.Value!;
         string? ifNoneMatch = context.Request.Headers.IfNoneMatch.FirstOrDefault()?.Replace("\"", "");
+        string? ifMatch = context.Request.Headers.IfMatch.FirstOrDefault()?.Replace("\"", "");
+
+        if (ConcurrencyCheckMethods.Contains(context.Request.Method) && !string.IsNullOrEmpty(ifMatch))
+        {
+            string? currentTag = etagStore.GetETag(resourceUri);
+
+            if (!string.IsNullOrEmpty(currentTag) && ifMatch != currentTag)
+            {
+                context.Response.StatusCode = StatusCodes.Status412PreconditionFailed;
+                context.Response.ContentLength = 0;
+                return;
+            }
+        }
 
         Stream originalStream = context.Response.Body;
         using var memoryStream = new MemoryStream();
