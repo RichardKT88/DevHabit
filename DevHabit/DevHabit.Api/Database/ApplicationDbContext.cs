d902910f18f0cc6aed3a03040a7bd9ce27d0e5b2
Richard Tanaka <kendy_t88@hotmail.com>
2025-07-15 18:28:15 -0300

    [6] - Building Asynchronous REST APIs - Finished

--- DevHabit/DevHabit.Api/Database/ApplicationDbContext.cs
+++ DevHabit/DevHabit.Api/Database/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
     public DbSet<User> Users { get; set; }
     public DbSet<GitHubAccessToken> GitHubAccessTokens { get; set; }
     public DbSet<Entry> Entries { get; set; }
+    public DbSet<EntryImportJob> EntryImportJobs { get; set; }
+
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
